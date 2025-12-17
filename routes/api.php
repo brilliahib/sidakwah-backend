36bef140b0664a1fff22a897e11f7abf84499d33
brilliahib <brilliahib21@gmail.com>
2025-12-17 15:58:49 +0700

    feat(modules): add controller for modules and role middleware with specific roles

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ModulController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,9 +9,27 @@ use Illuminate\Support\Facades\Route;
 Route::post('/auth/register', [AuthController::class, 'register']);
 Route::post('/auth/login', [AuthController::class, 'login']);
 
+// Public module routes
+Route::get('/modules/latest', [ModulController::class, 'getLatestModuls']);
+
 Route::middleware('auth:api')->group(function () {
     // Authenticated user routes
     Route::prefix('auth')->group(function () {
         Route::get('/get-auth', [AuthController::class, 'getAuth']);
+    });
+
+    // Module routes for users
+    Route::prefix('modules')->group(function () {
+        Route::get('/', [ModulController::class, 'index']);
+        Route::get('/{id}', [ModulController::class, 'show']);
+    });
+
+    Route::middleware('role:admin')->group(function () {
+        // Module routes for admin
+        Route::prefix('modules')->group(function () {
+            Route::post('/', [ModulController::class, 'store']);
+            Route::put('/{id}', [ModulController::class, 'update']);
+            Route::delete('/{id}', [ModulController::class, 'destroy']);
+        });
     });
 });
